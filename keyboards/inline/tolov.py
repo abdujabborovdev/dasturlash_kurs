d73fdd8fb9816c2dfe44f326ea8100944cf41247
Biloliddin <bilolidding@gmail.com>
2026-07-11 13:26:40 +0500

    First commit

--- keyboards/inline/tolov.py
+++ keyboards/inline/tolov.py
@@ -2,6 +2,5 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 tolov_qildim = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="tolov qildim",icon_custom_emoji_id="5427009714745517609",callback_data="tolov_qildim")],
-    [InlineKeyboardButton(text="Bosh menu", callback_data="bosh_menu", icon_custom_emoji_id='5255703720078879038')]
 ])
 
